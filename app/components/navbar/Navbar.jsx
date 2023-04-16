1b47db4533df5ad5f4aeeadccdbe723759f33821
EugeniBykovski <bykovskieugeni@mail.ru>
2023-04-16 13:12:52 +0200

    created components, updated styles

---
+++ app/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { memo } from 'react'
+import Container from '../Container'
+import Logo from '../logo/Logo'
+import Search from '../search/Search'
+import UserMenu from '../userMenu/UserMenu'
+
+const Navbar = memo(() => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="py-4 border-b-[1px]"
+      >
+        <Container>
+          <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  )
+})
+
+export default Navbar
---
+++ app/components/userMenu/UserMenu.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { memo, useCallback, useState } from "react"
+import { AiOutlineMenu } from 'react-icons/ai'
+import Avatar from "../Avatar"
+import MenuItem from "../menuItem/MenuItem"
+
+const UserMenu = memo(() => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleOpen = useCallback(() => setIsOpen(value => !value), [setIsOpen])
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div 
+          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full transition cursor-pointer hover:bg-neutral-100"
+        > 
+          Airbnb your home
+        </div>
+        <div
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer transition hover:shadow-md"
+          onClick={toggleOpen}
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem 
+                onClick={() => {}}
+                label="Login"
+              />
+              <MenuItem 
+                onClick={() => {}}
+                label="Sign Up"
+              />
+            </>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+})
+
+export default UserMenu
